3ccb421ee149d5e4b36ca859203832e7aeef808d
vaibhav khatal <vaibhavkhatal323@gmail,com>
2025-04-26 00:02:34 +0530

    files added

--- blogs/context_processors.py
+++
@@ -1,7 +0,0 @@
-
-from .models import Category
-
-def get_category(request):
-    # Fetch all categories or modify as per your need
-    categories = Category.objects.all()  # Fetching all categories
-    return {'categories': categories}
